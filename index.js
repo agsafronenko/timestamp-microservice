829d12e04d28e7bb78f682f6e7e529a036576b2b
agsafronenko <109076625+agsafronenko@users.noreply.github.com>
2023-04-11 15:19:13 +0200

    timestamp microservice

--- index.js
+++ index.js
@@ -12,18 +12,30 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
-// new comment
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 // your first API endpoint...
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+app.get("/api/:timestamp", (req, res) => {
+  let timestamp = req.params.timestamp;
+  let unix, utc;
+  if (/^\d{5,}$/.test(timestamp)) {
+    unix = Number(timestamp);
+    utc = new Date(Number(timestamp)).toUTCString();
+  }
+  if (/^\d{4}-\d{2}-\d{2}/.test(timestamp)) {
+    unix = new Date(timestamp).getTime();
+    utc = new Date(timestamp);
+  }
+
+  res.json({ unix: unix, utc: utc });
 });
 
+// {"unix":1451001600000,"utc":"Fri, 25 Dec 2015 00:00:00 GMT"}
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(5090, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
